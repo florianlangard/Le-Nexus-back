dadc9e9930f4a4bc635b4e99b6038162dc59ae1f
florianlangard <florian.langard@gmail.com>
2021-08-17 15:07:59 +0200

    User entity created

---
+++ src/Entity/User.php
@@ -0,0 +1,270 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * @ORM\Entity(repositoryClass=UserRepository::class)
+ */
+class User implements UserInterface, PasswordAuthenticatedUserInterface
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=180, unique=true)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="json")
+     */
+    private $roles = [];
+
+    /**
+     * @var string The hashed password
+     * @ORM\Column(type="string")
+     */
+    private $password;
+
+    /**
+     * @ORM\Column(type="string", length=32, unique=true)
+     */
+    private $pseudo;
+
+    /**
+     * @ORM\Column(type="integer", unique=true)
+     */
+    private $steamId;
+
+    /**
+     * @ORM\Column(type="string", length=64, unique=true)
+     */
+    private $steamUsername;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $steamAvatar;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $visibilityState;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $isLogged;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $updatedAt;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->email;
+    }
+
+    /**
+     * @deprecated since Symfony 5.3, use getUserIdentifier instead
+     */
+    public function getUsername(): string
+    {
+        return (string) $this->email;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Returning a salt is only needed, if you are not using a modern
+     * hashing algorithm (e.g. bcrypt or sodium) in your security.yaml.
+     *
+     * @see UserInterface
+     */
+    public function getSalt(): ?string
+    {
+        return null;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+
+    public function getSteamId(): ?int
+    {
+        return $this->steamId;
+    }
+
+    public function setSteamId(int $steamId): self
+    {
+        $this->steamId = $steamId;
+
+        return $this;
+    }
+
+    public function getSteamUsername(): ?string
+    {
+        return $this->steamUsername;
+    }
+
+    public function setSteamUsername(string $steamUsername): self
+    {
+        $this->steamUsername = $steamUsername;
+
+        return $this;
+    }
+
+    public function getSteamAvatar(): ?string
+    {
+        return $this->steamAvatar;
+    }
+
+    public function setSteamAvatar(?string $steamAvatar): self
+    {
+        $this->steamAvatar = $steamAvatar;
+
+        return $this;
+    }
+
+    public function getVisibilityState(): ?bool
+    {
+        return $this->visibilityState;
+    }
+
+    public function setVisibilityState(bool $visibilityState): self
+    {
+        $this->visibilityState = $visibilityState;
+
+        return $this;
+    }
+
+    public function getIsLogged(): ?bool
+    {
+        return $this->isLogged;
+    }
+
+    public function setIsLogged(bool $isLogged): self
+    {
+        $this->isLogged = $isLogged;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(?\DateTimeInterface $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of pseudo
+     */ 
+    public function getPseudo() :string
+    {
+        return $this->pseudo;
+    }
+
+    /**
+     * Set the value of pseudo
+     *
+     * @return  self
+     */ 
+    public function setPseudo(string $pseudo) :self
+    {
+        $this->pseudo = $pseudo;
+
+        return $this;
+    }
+}
