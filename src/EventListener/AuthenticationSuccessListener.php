62f260f6d8fa9aeea0021495d7cbd0b97c6cfc26
florianlangard <florian.langard@gmail.com>
2021-08-23 11:52:44 +0200

    added AuthenticationSuccessListener for custom login response

---
+++ src/EventListener/AuthenticationSuccessListener.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\EventListener;
+
+use Lexik\Bundle\JWTAuthenticationBundle\Event\AuthenticationSuccessEvent;
+use App\Entity\User;
+
+class AuthenticationSuccessListener
+{
+    public function onAuthenticationSuccessResponse(AuthenticationSuccessEvent $event)
+    {
+        // $data = $event->getData(); yields $data['token'] = <the token>
+        $authenticatedUserId = $event->getUser()->getSteamId();
+        // $person_id = $authenticatedUser->getSteamId();
+        // dd($authenticatedUserId);
+        // $person_id = $person->getId();
+
+
+        $event->setData([
+            // 'code' => $event->getResponse()->getStatusCode(),
+            'payload' => $event->getData(),
+            'authenticatedUserId' => $authenticatedUserId,
+        ]);
+    }
+}
