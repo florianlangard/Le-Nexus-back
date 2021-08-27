ddbc4d2b061668415991446372bf4cc78a2fb550
florianlangard <florian.langard@gmail.com>
2021-08-27 20:44:49 +0200

    wip backOffice user

---
+++ src/Form/UserType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email')
+            ->add('roles', ChoiceType::class, [
+                'choices' => [
+                    'membre' => 'ROLE_USER',
+                    'Administrateur' => 'ROLE_ADMIN'
+                ],
+                'multiple' => true,
+                'expanded' => true,
+            ])
+            ->add('pseudo')
+            // ->add('password')
+            // ->add('steamId')
+            //->add('steamUsername')
+            //->add('steamAvatar')
+            //->add('visibilityState')
+            // ->add('isLogged')
+            // ->add('createdAt')
+            // ->add('updatedAt')
+            //->add('mood')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
