3f556d7cb66f31f648bb91a5d75cd614190b062b
julien-gassmann <juliengassmann974@gmail.com>
2021-08-28 15:20:12 +0200

    Api/RequestController : ok (all conditions fixed)

--- src/Controller/Api/RequestController.php
+++ src/Controller/Api/RequestController.php
@@ -6,6 +6,8 @@ use App\Entity\User;
 use App\Repository\UserRepository;
 use App\Repository\RequestRepository;
 use App\Entity\Request as EntityRequest;
+use App\Repository\FriendshipRepository;
+use App\Repository\LibraryRepository;
 use App\Service\ResponseHandler;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -23,15 +25,21 @@ class RequestController extends AbstractController
     /**
      * @Route("/api/request", name="api_request_send", methods={"POST"})
      */
-    public function sendRequest(Request $request, ValidatorInterface $validator, SerializerInterface $serializer, EntityManagerInterface $em, UserRepository $userRepository): Response
+    public function sendRequest(FriendshipRepository $friendshipRepository, LibraryRepository $libraryRepository, Request $request, ValidatorInterface $validator, SerializerInterface $serializer, EntityManagerInterface $em, UserRepository $userRepository): Response
     {
         $jsonContent = $request->getContent();
 
         $newRequest = $serializer->deserialize($jsonContent, EntityRequest::class, 'json');
 
         if ($newRequest->getSender() === $this->getUser() && $newRequest->getType() === 'friend') {
+
+            // If Users are already friends
+            if ($friendshipRepository->findOneByUserAndFriend($this->getUser(), $newRequest->getTarget())) {
+                return $this->json('You are already friend with these user', Response::HTTP_FORBIDDEN);
+            }
+
             $errors = $validator->validate($newRequest);
-        
+
             if (count($errors) > 0) {
                 $errorsString = (string) $errors;
             
@@ -42,44 +50,39 @@ class RequestController extends AbstractController
 
             return $this->json($newRequest, Response::HTTP_CREATED, [], ['groups' => 'request_info']);
 
-        } elseif ($newRequest->getSender() === $this->getUser() && $newRequest->getType() === 'game') {
+        } 
+        elseif ($newRequest->getSender() === $this->getUser() && $newRequest->getType() === 'game') {
 
-            // // If the user connected as in his frendslist the user set in the target property of the request
-            // if ($this->getUser()->getFriends()->findOneByUserAndFriend($this->getUser(), $newRequest->getSender())) {
-
-            //     // If the user connected has not the game in his library
-            //     if (!$this->getUser()->getLibraries()->findOneByGameAndUser($newRequest->getGame(), $this->getUser())) {
-            //         return $this->json('You must have these game in your library to send an invitation', Response::HTTP_FORBIDDEN);
-            //     }
-            //     // If the target of the request has not the game in his library
-            //     if (!$newRequest->getTarget()->getLibraries()->findOneByGameAndUser($newRequest->getGame(), $newRequest->getTarget())) {
-            //         return $this->json('You friend must have these game in your library to send an invitation', Response::HTTP_FORBIDDEN);
-                
-            
-
-            foreach ($this->getUser()->getFriends() as $currentFriendship) {
-
-                $currentfriend = $currentFriendship->getFriend();
-    
-                if ($newRequest->getTarget() === $currentfriend) {
-
-                $errors = $validator->validate($newRequest);
-    
-                    if (count($errors) > 0) {
-                        $errorsString = (string) $errors;
-                    
-                        return $this->json(['errors' => $errors], Response::HTTP_UNPROCESSABLE_ENTITY);
-                    }
-                    $em->persist($newRequest);
-                    $em->flush();
-        
-                    return $this->json($newRequest, Response::HTTP_CREATED, [], ['groups' => 'request_info']);
-                }
+            // If the user connected has not in his frendslist the user set in the target property of the request
+            if (!$friendshipRepository->findOneByUserAndFriend($this->getUser(), $newRequest->getTarget())) {
+                return $this->json('You must be friend with these user to send an invitation', Response::HTTP_FORBIDDEN);
             }
+            // If the user connected has not the game in his library
+            if (!$libraryRepository->findOneByGameAndUser($newRequest->getGame(), $this->getUser())) {
+                return $this->json('You must have these game in your library to send an invitation', Response::HTTP_FORBIDDEN);
+            }
+            // If the target of the request has not the game in his library
+            if (!$libraryRepository->findOneByGameAndUser($newRequest->getGame(), $newRequest->getTarget())) {
+                return $this->json('You friend must have these game in your library to send an invitation', Response::HTTP_FORBIDDEN);
+            }
+
+            $errors = $validator->validate($newRequest);
+
+            if (count($errors) > 0) {
+                $errorsString = (string) $errors;
+            
+                return $this->json(['errors' => $errors], Response::HTTP_UNPROCESSABLE_ENTITY);
+            }
+            $em->persist($newRequest);
+            $em->flush();
+
+            return $this->json($newRequest, Response::HTTP_CREATED, [], ['groups' => 'request_info']);           
         }
         
         return $this->json([], Response::HTTP_FORBIDDEN);
     }
+        
+        
 
     /**
      * @Route("/api/request/{id}", name="api_request_answer", methods={"PATCH"})
